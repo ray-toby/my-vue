46c23ff8d368d99765522cc3703b85d52581a8c0
fei.lei <fei.lei@audaque.com>
2020-04-15 17:34:17 +0800

    vue router

---
+++ router/src/views/About.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <h1>{{$route.params.id}}</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    created() {
+      console.log(this.$route)
+    },
+  }
+</script>
